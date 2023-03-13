d19af64d6127c92a7fea29135bdbe6bd50687e62
NelloRossi97 <rossianiello97@gmail.com>
2023-03-13 15:33:36 +0100

    added dice, email html and function control email

--- js/script.js
+++ js/script.js
@@ -22,3 +22,47 @@ Consigli del giorno:
 4. torniamo a scrivere in italiano
 5. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano" 
 */
+
+const btnEmail = document.getElementById('email');
+const btnDice = document.getElementById('dice');
+const btnSubmit = document.getElementById('submit');
+let emailAllowed = [
+    'cleliafradella@gmail.com',
+    'marcoacciari@gmail.com',
+    'samuelpanicucci@gmail.com',
+    'bryanlucchetta@gmail',
+    'adrianogrimaldi@gmail.com',
+    'eugeniafaiola@gmail.com'
+]
+
+if (btnEmail !== null){
+    btnEmail.addEventListener('click', function(){
+    window.open("email.html");
+})
+}
+
+if (btnDice !== null){
+    btnDice.addEventListener('click', function(){
+    window.open("dice.html");
+})
+}
+
+if(btnSubmit !== null){
+    btnSubmit.addEventListener('click', function(reload){
+    reload.preventDefault()
+})
+} 
+
+btnSubmit.addEventListener('click', function(){
+    emailBox = document.getElementById('emailBox').value;
+    emailResult = document.getElementById('emailResult');
+    for (i = 0; i < emailAllowed.length; i++){
+        if (emailBox === emailAllowed[i]){
+            console.log('accettato');
+            emailResult.innerHTML = `<h2 class="text-success"> L'email è accettata </h2>`
+        } else{
+            emailResult.innerHTML = `<h2 class="text-danger"> L'email non è accettata </h2>`;
+            console.log('non accettato');
+        }
+    }
+})
